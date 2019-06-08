f0dc1b03d7267908529a609e49044001f5e67ab9
Minecrell <minecrell@minecrell.net>
2019-06-08 16:50:45 +0200

    Update JLine to 3.11.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,8 +21,8 @@ dependencies {
     compile("org.apache.logging.log4j:log4j-core:2.8.1")
     annotationProcessor("org.apache.logging.log4j:log4j-core:2.8.1")
 
-    compile("org.jline:jline-reader:3.10.0")
-    runtime("org.jline:jline-terminal-jna:3.10.0")
+    compile("org.jline:jline-reader:3.11.0")
+    runtime("org.jline:jline-terminal-jna:3.11.0")
 
     compileOnly("com.google.code.findbugs:jsr305:3.0.2")
 
