804f6e3dde38791336fe7bc75ae21bfa28f035e0
Tobias Hotz <tobias.hotz@hotmail.de>
2020-06-04 12:20:09 +0200

    Documentation andfix for arg parsing

--- src/main/java/net/minecrell/terminalconsole/TCAHighlightConverter.java
+++ src/main/java/net/minecrell/terminalconsole/TCAHighlightConverter.java
@@ -11,8 +11,23 @@ import org.apache.logging.log4j.util.PerformanceSensitive;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+/**
+ * A wrapped version of {@link HighlightConverter} that uses
+ * {@link TerminalConsoleAppender} to detect if Ansi escape codes can be used
+ * to highlight errors and warnings in the console.
+ *
+ * <p>If configured, it will mark using the colors from {@link HighlightConverter}.
+ * It can be only used together with
+ * {@link TerminalConsoleAppender}.</p>
+ *
+ * <p>{@link TerminalConsoleAppender#ANSI_OVERRIDE_PROPERTY} may be used
+ * to force the use of ANSI colors even in unsupported environments.</p>
+ *
+ * <p><b>Example usage:</b> {@code %TCAhighlight{%level: %message}}</p>
+ */
 @Plugin(name = "TCAhighlight", category = PatternConverter.CATEGORY)
 @ConverterKeys({ "TCAhighlight" })
 @PerformanceSensitive("allocation")
@@ -20,26 +35,19 @@ public class TCAHighlightConverter {
     protected static final Logger LOGGER = StatusLogger.getLogger();
 
     /**
-     * Gets a new instance of the {@link HighlightErrorConverter} with the
+     * Gets a new instance of the {@link HighlightConverter} with the
      * specified options.
+     * The difference to the vanilla {@link HighlightConverter} is that
+     * ANSI is disabled automatically when no terminal is present.
      *
      * @param config The current configuration
      * @param options The pattern options
      * @return The new instance
      */
     public static @Nullable HighlightConverter newInstance(Configuration config, String[] options) {
-        if (options.length != 1) {
-            LOGGER.error("Incorrect number of options on highlightError. Expected 1 received " + options.length);
-            return null;
-        }
-        if (options[0] == null) {
-            LOGGER.error("No pattern supplied on highlightError");
-            return null;
-        }
 
         if (!TerminalConsoleAppender.isAnsiSupported()) {
-            List<String> optionList = new ArrayList<>();
-            optionList.add(options[0]);
+            List<String> optionList = new ArrayList<>(Arrays.asList(options));
             optionList.add("disableAnsi=true");
             options = optionList.toArray(new String[0]);
         }
