54ab86e9875bc47fe4a58b130d532976637b171f
Minecrell <minecrell@minecrell.net>
2021-05-07 20:36:45 +0200

    Update Gradle wrapper to 7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    java
+    `java-library`
     `maven-publish`
     signing
-    id("io.codearte.nexus-staging") version "0.21.0"
-    id("net.minecrell.licenser") version "0.4.1"
+    id("io.codearte.nexus-staging") version "0.30.0"
+    id("org.cadixdev.licenser") version "0.6.0"
 }
 
 val artifactId = project.name.toLowerCase()
@@ -18,29 +18,24 @@ repositories {
 }
 
 dependencies {
-    compile("org.apache.logging.log4j:log4j-core:2.8.1")
+    api("org.apache.logging.log4j:log4j-core:2.8.1")
     annotationProcessor("org.apache.logging.log4j:log4j-core:2.8.1")
 
-    compile("org.jline:jline-reader:3.12.1")
+    api("org.jline:jline-reader:3.12.1")
 
     compileOnly("org.checkerframework:checker-qual:2.9.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.1")
-    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.5.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.1")
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
-}
-
-val sourceJar = task<Jar>("sourceJar") {
-    archiveClassifier("sources")
-    from(sourceSets["main"].allSource)
-}
-
-val javadocJar = task<Jar>("javadocJar") {
-    archiveClassifier("javadoc")
-    from(tasks["javadoc"])
 }
 
 val isSnapshot = version.toString().endsWith("-SNAPSHOT")
@@ -50,9 +45,6 @@ publishing {
         register<MavenPublication>("mavenJava") {
             from(components["java"])
             artifactId = base.archivesBaseName
-
-            artifact(sourceJar)
-            artifact(javadocJar)
 
             pom {
                 val url: String by project
