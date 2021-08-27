c8cd166f5a74e383d9e66b8cf06c0b4e2c511862
Minecrell <minecrell@minecrell.net>
2021-08-27 13:44:33 +0200

    build: Update checker-qual dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
 
     api("org.jline:jline-reader:3.20.0")
 
-    compileOnly("org.checkerframework:checker-qual:3.13.0")
+    compileOnly("org.checkerframework:checker-qual:3.17.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
